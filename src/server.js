d59647a672d206a15562aa3b2c647b59a930d761
NasiPecel11 <galihprakoso126@gmail.com>
2022-03-28 20:40:20 +0700

    membuat musik apps

---
+++ src/server.js
@@ -0,0 +1,50 @@
+require("dotenv").config();
+
+const Hapi = require("@hapi/hapi");
+
+// Album
+const album = require("./api/album");
+const AlbumService = require("./services/postgres/AlbumService");
+const AlbumValidator = require("./validator/album");
+
+// Song
+const song = require("./api/song");
+const SongService = require("./services/postgres/SongService");
+const SongValidator = require("./validator/song");
+
+const init = async () => {
+  const albumService = new AlbumService();
+  const songService = new SongService();
+
+  const server = Hapi.server({
+    port: process.env.PORT,
+    host: process.env.HOST,
+    routes: {
+      cors: {
+        origin: ["*"],
+      },
+    },
+  });
+
+  await server.register([
+    {
+      plugin: album,
+      options: {
+        service: albumService,
+        validator: AlbumValidator,
+      },
+    },
+    {
+      plugin: song,
+      options: {
+        service: songService,
+        validator: SongValidator,
+      },
+    },
+  ]);
+
+  await server.start();
+  console.log(`Server berjalan pada ${server.info.uri}`);
+};
+
+init();
